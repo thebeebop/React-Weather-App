6dd1cbaf5a1ba03f2915d1e87c633c716c954e00
Lewis Woods <thebebop@The-Beebop.local>
2022-12-21 12:03:25 +0000

    Finished axios set-up. Created getter API functions.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import Search from "./components/Search";
 import MainDisplay from "./components/MainDisplay";
 import { useState, useEffect } from "react";
+import { getGeoCodes, getWeatherData } from "./utils/api-caller.js";
 
 function App() {
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState("liverpool");
   const [error, setError] = useState(null);
   const [weatherObj, setWeatherObj] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {}, [location]);
+  useEffect(() => {
+    setIsLoading(true);
+    getGeoCodes(location).then((response) => {
+      const lat = response.data[0].lat;
+      const lon = response.data[0].lon;
+      getWeatherData(lat, lon).then((response) => {
+        setWeatherObj(response.data);
+        console.log(weatherObj, "<<<weatherObj");
+        setIsLoading(false);
+      });
+    });
+  }, [location]);
 
-  // if (isLoading) {
-  //   return <p>Loading...</p>;
-  // } else {
-  return (
-    <div className="App">
-      <Search location={location} setLocation={setLocation} />
-      <MainDisplay weatherObj={weatherObj} />
-    </div>
-  );
+  if (isLoading) {
+    return (
+      <div className="App">
+        <Search location={location} setLocation={setLocation} />
+        <p>Loading...</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <Search location={location} setLocation={setLocation} />
+        <MainDisplay weatherObj={weatherObj} />
+      </div>
+    );
+  }
 }
-// }
 
 export default App;
---
+++ src/utils/api-caller.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const apiKey = "b89c2f4cd67785bc98a5ce98f95147b1";
+
+function getGeoCodes(location) {
+  return axios
+    .get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`
+    )
+    .then((response) => {
+      return response;
+    });
+}
+
+function getWeatherData(lat, lon) {
+  return axios
+    .get(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&appid=${apiKey}`
+    )
+    .then((response) => {
+      return response;
+    });
+}
+export { getGeoCodes, getWeatherData };
